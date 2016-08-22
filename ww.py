de92d17cf37d470d5184b4d80db2acf5cf233248
Youri Ackx <youri@ackx.net>
2016-08-22 14:39:00 +0200

    Parse filename from cmd line

--- ww.py
+++ ww.py
@@ -3,12 +3,17 @@
 from engine import Engine
 from gui import Gui
 
+import argparse
 
-def main():
-    initial_state = Engine.load('diode.txt')
+
+def main(path):
+    initial_state = Engine.load(path)
     ww = Engine(initial_state)
     Gui(ww).start()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser('Wireworld animation')
+    parser.add_argument('world', help='File path to a wireworld')
+    args = parser.parse_args()
+    main(args.world)
